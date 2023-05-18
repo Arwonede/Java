e7368ab2d0778e5c5b9e4637ddf279212cf57b9f
Arwonede <108694415+Arwonede@users.noreply.github.com>
2023-05-18 22:03:53 +0200

    HomeLearning
    Spring and Apache Tomcat

---
+++ HomeLearning/firstStep/src/main/java/pl/arwonede/firstStep/Config.java
@@ -0,0 +1,24 @@
+package pl.arwonede.firstStep;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@ComponentScan("pl.arwonede.firstStep") //definiuje pakiet w którym będzie zarządzać klasami
+public class Config extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
